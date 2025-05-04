bda1f1f9b0fd1be1b4fe18095155073291925d1a
duncte123 <contact@duncte123.me>
2025-05-04 18:48:27 +0200

    Add event lookup api

--- src/Controller/Api/Version1/EventController.php
+++ src/Controller/Api/Version1/EventController.php
@@ -4,9 +4,15 @@ namespace App\Controller\Api\Version1;
 
 use App\Controller\Api\BaseController;
 use App\Entity\Event;
+use App\Entity\Schedule;
+use App\Horaro\Ex\EventNotFoundException;
+use App\Horaro\Ex\ScheduleNotFoundException;
+use App\Horaro\Library\ObscurityCodec;
 use App\Horaro\Pager\OffsetLimitPager;
 use App\Horaro\Transformer\Version1\EventTransformer;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EventController extends BaseController
@@ -46,7 +52,77 @@ class EventController extends BaseController
         $events = $queryBuilder->getQuery()->getResult();
 
         $transformer = new EventTransformer($this->requestStack, $this->obscurityCodec);
+
         return $this->respondWithCollection($events, $transformer, $pager);
     }
 
+    // We love custom lookups xD
+    // Might refactor this in the future
+    // But we all know there is nothing as permanent as a temporary solution
+    #[Route('/-/api/v1/events/{eventid}', name: 'app_api_v1_event_view', methods: ['GET'])]
+    public function viewEvent(Request $request): Response {
+        [$event, $bySlug] = $this->resolveEvent($request);
+
+        if ($bySlug) {
+            $id = $this->encodeID($event->getID(), ObscurityCodec::EVENT);
+
+            return $this->redirect('/-/api/v1/events/'.$id);
+        }
+
+        $transformer = new EventTransformer($this->requestStack, $this->obscurityCodec);
+
+        return $this->respondWithItem($event, $transformer);
+    }
+
+    private function resolveEvent(Request $request): array
+    {
+        $event = null;
+        $bySlug = false;
+        $eventID = $request->attributes->get('eventid');
+        $codec = $this->obscurityCodec;
+        $repo = $this->entityManager->getRepository(Event::class);
+        $id = $codec->decode($eventID, ObscurityCodec::EVENT);
+
+        if ($id === null) {
+            $bySlug = true;
+            $event = $repo->findOneBy(['slug' => $eventID]);
+        } else {
+            $event = $repo->find($id);
+        }
+
+        if (!$event || !$event->isPublic()) {
+            throw new EventNotFoundException();
+        }
+
+        return [$event, $bySlug];
+    }
+
+    private function resolveSchedule(Event $event, Request $request): array
+    {
+        $schedule = null;
+        $bySlug = false;
+        $scheduleID = $request->attributes->get('scheduleid');
+        $codec = $this->obscurityCodec;
+        $repo = $this->entityManager->getRepository(Schedule::class);
+        $id = $codec->decode($scheduleID, ObscurityCodec::SCHEDULE);
+
+        if ($id === null) {
+            $bySlug = true;
+            $schedule = $repo->findOneBy([
+                'event' => $event,
+                'slug' => $scheduleID,
+            ]);
+        } else {
+            $schedule = $repo->findOneBy([
+                'event' => $event,
+                'id' => $id,
+            ]);
+        }
+
+        if (!$schedule || !$schedule->isPublic()) {
+            throw new ScheduleNotFoundException();
+        }
+
+        return [$schedule, $bySlug];
+    }
 }
