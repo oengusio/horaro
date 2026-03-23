fbdede9fad15f401a120ff3a88733434928f5041
duncte123 <contact@duncte123.me>
2026-03-23 17:27:36 +0100

    Trying a different sollution

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,6 @@ set -e
 
 # make sure mounted directories have proper permissions
 chown horaro:horaro tmp/upload tmp/session log
-# nginx runs has the horaro user, needs access to these files
-mkdir -p /var/lib/nginx/tmp
-chmod -R 666 /var/lib/nginx/tmp/
 
 # hand control over to supervisord
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
