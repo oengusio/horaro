edecbbc8ab5db786632f88d0d763baceee18c344
duncte123 <contact@duncte123.me>
2026-08-29 11:45:38 +0200

    Proper autofocus

--- assets/vue/components/HoraroEditor.js
+++ assets/vue/components/HoraroEditor.js
@@ -1,8 +1,16 @@
 import { ref } from 'vue';
 
+const focus = {
+  mounted: (el) => el.focus()
+}
+
 export default {
   props: ['modelValue'],
   emits: ['update:modelValue'],
+   directives: {
+    // enables v-focus in template
+    focus,
+  },
   setup(props, { emit }) {
     const isOpen = ref(false);
     const inputCache = ref(props.modelValue);
@@ -28,7 +36,7 @@ export default {
   // language=vue
   template: `<div class="h-editor">
     <div class="input-holder" v-if="isOpen">
-      <input type="text" v-model="inputCache" autofocus>
+      <input v-focus type="text" name="input" v-model.trim="inputCache" @keydown.enter="saveValue" @keydown.esc="cancelEdit" />
       <button type="button" @click.prevent="saveValue">save</button>
       <button type="button" @click.prevent="cancelEdit">cancel</button>
     </div>
